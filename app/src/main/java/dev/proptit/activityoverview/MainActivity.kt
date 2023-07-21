3678aaa5900d3fa97327381d26a940185471feb1
mdunggggg <dungmanh74@gmail.com>
2023-07-22 00:12:21 +0700

    Design

--- app/src/main/java/dev/proptit/activityoverview/MainActivity.kt
+++ app/src/main/java/dev/proptit/activityoverview/MainActivity.kt
@@ -1,14 +1,42 @@
 package dev.proptit.activityoverview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import dev.proptit.activityoverview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val REQUEST_CODE_REGISTER_EMAIL = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        binding.btnLogin.setOnClickListener {
+            if (binding.etEmail.text.toString().equals("") || binding.etPassword.text.toString()
+                    .equals("")
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Please enter your email and password",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                val intent = Intent(this@MainActivity, SendEmailActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+
+        binding.tvRegister.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_REGISTER_EMAIL)
+        }
     }
 }
--- app/src/main/java/dev/proptit/activityoverview/SendEmailActivity.kt
+++ app/src/main/java/dev/proptit/activityoverview/SendEmailActivity.kt
@@ -1,11 +1,43 @@
 package dev.proptit.activityoverview
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import dev.proptit.activityoverview.databinding.ActivitySendEmailBinding
 
 class SendEmailActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySendEmailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_send_email)
+        binding = ActivitySendEmailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnSend.setOnClickListener{
+            val receiver = binding.etEmailReceiver.text.toString()
+            val subject = binding.etSubject.text.toString()
+            val body = binding.etEmailBody.text.toString()
+            if(receiver.equals("") || subject.equals("") || body.equals("")){
+                Toast.makeText(this@SendEmailActivity, "Please enter all fields", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                sendEmail(receiver, subject, body)
+            }
+        }
+    }
+    private fun sendEmail(receiver: String, subject: String, body: String){
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.setData(Uri.parse("mailto:"))
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(receiver))
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, body)
+        if(intent.resolveActivity(packageManager) != null){
+            startActivity(intent)
+        }
+        else{
+            Toast.makeText(this@SendEmailActivity, "No app can handle this request", Toast.LENGTH_SHORT).show()
+        }
     }
 }
