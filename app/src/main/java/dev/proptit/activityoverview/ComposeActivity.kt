7ac69856ae77e5e4db1a736470abdcdad275b5af
TrisTaam <yadartcon@gmail.com>
2023-07-22 16:59:22 +0700

    Send mail function

--- app/src/main/java/dev/proptit/activityoverview/ComposeActivity.kt
+++ app/src/main/java/dev/proptit/activityoverview/ComposeActivity.kt
@@ -1,7 +1,10 @@
 package dev.proptit.activityoverview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import dev.proptit.activityoverview.databinding.ActivityComposeBinding
 
 class ComposeActivity : AppCompatActivity() {
@@ -14,5 +17,21 @@ class ComposeActivity : AppCompatActivity() {
         binding.toBox.fieldName.text = getString(R.string.to)
         binding.subjectBox.fieldName.text = getString(R.string.subject)
         binding.composeEmailBox.fieldName.text = getString(R.string.compose_email)
+        binding.btnSend.setOnClickListener {
+            sendMail()
+        }
+    }
+
+    private fun sendMail() {
+        val originalString = binding.toBox.box.text.toString()
+        val recipients = originalString.split(",").toTypedArray()
+        val subject = binding.subjectBox.box.text.toString()
+        val compose = binding.composeEmailBox.box.text.toString()
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients)
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, compose)
+        startActivity(intent)
     }
 }
