9426790d57fcb2cc8f0ccd4bf67e9c30b89351a3
LordierClaw <lordierclaw@gmail.com>
2023-07-22 18:47:48 +0700

    Finish basic logic

--- app/src/main/java/dev/proptit/activityoverview/LoginActivity.kt
+++ app/src/main/java/dev/proptit/activityoverview/LoginActivity.kt
@@ -1,11 +1,46 @@
 package dev.proptit.activityoverview
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import dev.proptit.activityoverview.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        val USER_EMAIL: String = "user_email"
+    }
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(USER_EMAIL, binding.loginEmailEdit.text)
+            startActivity(intent)
+        }
+
+        binding.loginToRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivityForResult(intent, RegisterActivity.REGISTER_THEN_LOGIN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RegisterActivity.REGISTER_THEN_LOGIN) {
+            if (resultCode == Activity.RESULT_OK) {
+                val userEmail = data?.getStringExtra(RegisterActivity.USER_EMAIL)
+                binding.loginEmailEdit.setText(userEmail)
+            }
+        }
     }
 }
--- app/src/main/java/dev/proptit/activityoverview/MainActivity.kt
+++ app/src/main/java/dev/proptit/activityoverview/MainActivity.kt
@@ -1,14 +1,31 @@
 package dev.proptit.activityoverview
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.proptit.activityoverview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.mainSendBtn.setOnClickListener {
+            // https://developer.android.com/guide/components/intents-common.html#Email
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, binding.mainToEdit.text)
+                putExtra(Intent.EXTRA_SUBJECT, binding.mainSubjectEdit.text)
+                putExtra(Intent.EXTRA_TEXT, binding.mainComposeEmailEdit.text)
+            }
+            startActivity(intent)
+        }
     }
+
+
 }
